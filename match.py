9d120f55ba93d7d998ce008c1ef8c45d3a5b89a6
crvstalphua <crystal180402@gmail.com>
2024-10-05 23:22:52 +0800

    Add match class

---
+++ match.py
@@ -0,0 +1,6 @@
+class Match:
+    def _init_(self, team_1, team_2, goals_1, goals_2):
+        self.team_1 = team_1
+        self.team_2 = team_2
+        self.goals_1 = goals_1
+        self.goals_2 = goals_2
